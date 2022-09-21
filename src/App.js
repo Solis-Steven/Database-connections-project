c697bdbb7df72d968a6a06309fdd45dd1c4b18ca
Steven Solis <stevenso@estudiantec.cr>
2022-09-20 19:21:59 -0600

    Background added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
-function App() {
+import StarfieldAnimation from "react-starfield-animation";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StarfieldAnimation
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+      }}
+      numParticles={1000}
+      particleSpeed={0}
+      dx={0.000000001} // x speed of stars in px/frame, default 0.05
+      dy={0.000000001}
+    />
   );
 }
-
-export default App;
---
+++ src/components/Form/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Form() {
+    return(
+        <form>
+            <label>Algo</label>
+            <input type="text"></input>
+            <label>Algo</label>
+            <input type="text"></input>
+            <button type="submit"></button>
+        </form>
+    );
+}
+
+export { Form };
