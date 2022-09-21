12fa98ea27ff6053bc04e1f7b3f9c6068f054334
Steven Solis <stevenso@estudiantec.cr>
2022-09-21 10:48:36 -0600

    Form data retrieval

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,22 +2,58 @@ import React from "react";
 import "./Form.css"
 
 function Form() {
-    return(
+    // Creamos el estado de nuestro formulario
+    const [ userNameState, setUserNameState ] = React.useState("");
+    const [ passwordState, setPasswordState ] = React.useState("");
+    const myForm = React.useRef( null );
 
-    <section className="container">
-        <div className="prueba">
-        
-            <form className="form">
-            <h2 className="title">Database connections</h2>
-                <label className="formulario-label">Username or address for the connection</label>
-                <input type="text" className="formulario-input" placeholder="Username"></input>
-                <label className="formulario-label">Password for the connection</label>
-                <input type="password" className="formulario-input" placeholder="Password"></input>
-                <input type="submit" className="formulario-button" value="Connect"></input>
-            </form>
-        </div>
-    </section>
-    );
+    const userNameChanges = (e) => {
+       setUserNameState(e.target.value); 
+    }
+
+    const passwordChanges = (e) => {
+        setPasswordState(e.target.value); 
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(userNameState);
+        console.log(passwordState);
+        setUserNameState("");
+        setPasswordState("");
+    }
+
+    return(
+        <section className="container">
+            <div className="prueba">
+            
+                <form ref={myForm} className="form" onSubmit={onSubmit}>
+                <h2 className="title">Database connections</h2>
+                    <label className="formulario-label">Username for the connection</label>
+                    <input 
+                        type="text"
+                        className="formulario-input" 
+                        placeholder="Username" 
+                        name="userName"
+                        value={userNameState}
+                        onChange={userNameChanges}>
+                    </input>
+
+                    <label className="formulario-label">Password for the connection</label>
+                    <input 
+                        type="password" 
+                        className="formulario-input" 
+                        placeholder="Password"
+                        name="password"
+                        value={passwordState}
+                        onChange={passwordChanges}>
+                    </input>
+                    
+                    <input type="submit" className="formulario-button" value="Connect"></input>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export { Form };
